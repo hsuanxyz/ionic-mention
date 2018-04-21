e7a068ec9a5d9f44086536af790bb6874b64bcd9
Hsuan Lee <HsuanGM@gmail.com>
2018-04-21 23:06:50 +0800

    bind host input event

--- src/ionic-mention/mention-trigger.ts
+++ src/ionic-mention/mention-trigger.ts
@@ -1,11 +1,19 @@
 import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[ionicMention]'
+  selector: '[ionicMention]',
+  host: {
+    '(input)': 'onInput($event)'
+  }
 })
 export class MentionTrigger {
 
   constructor(elementRef: ElementRef) {
     console.log(elementRef.nativeElement.tagName)
+  }
+
+  onInput($event) {
+    const target: HTMLInputElement | HTMLTextAreaElement = $event.target;
+    console.log(target.value);
   }
 }
