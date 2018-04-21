c2cc7bdfc357fed961a195d96c0d8543eca86645
Hsuan Lee <HsuanGM@gmail.com>
2018-04-22 00:13:59 +0800

    add output propertys

--- src/ionic-mention/mention-trigger.ts
+++ src/ionic-mention/mention-trigger.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IonicMentionSuggestionModal } from './mention-suggestion.interface';
 
 @Directive({
@@ -10,6 +10,10 @@ import { IonicMentionSuggestionModal } from './mention-suggestion.interface';
 export class MentionTrigger implements OnInit {
 
   @Input('ionicMention') suggestionModal: IonicMentionSuggestionModal;
+  @Input() prefix: string | string[] = '@';
+  @Output() onOpen: EventEmitter<string> = new EventEmitter();
+  @Output() onSelected: EventEmitter<any> = new EventEmitter();
+  @Output() onCancel: EventEmitter<void> = new EventEmitter();
 
   constructor(public elementRef: ElementRef) {
     console.log(this.elementRef.nativeElement.tagName);
